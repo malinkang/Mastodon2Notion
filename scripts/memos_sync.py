fe18cf9317d520838239599e1034fb942c63ac19
malinkang <malinkang@adsconflux.com>
2024-10-23 11:42:30 +0800

    fix bug

--- scripts/memos_sync.py
+++ scripts/memos_sync.py
@@ -21,7 +21,7 @@ def authenticate_user():
     }
 
     response = requests.post(url, json=data)
-    print(response)
+    print(response.text)
     if response.status_code == 200:
         return response.cookies
     else:
